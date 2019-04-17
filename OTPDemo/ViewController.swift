1e8d7e6ee7d789b09a96c96e57e8989206394aec
admin <mobileteam@syneoteksoft.com>
2019-04-17 11:05:04 +0530

    first commit

---
+++ OTPDemo/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  OTPDemo
+//
+//  Created by Shailesh on 17/04/19.
+//  Copyright © 2019 Shailesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var txtOTP1: UITextField!
+    @IBOutlet weak var txtOTP2: UITextField!
+    @IBOutlet weak var txtOTP3: UITextField!
+    @IBOutlet weak var txtOTP4: UITextField!
+    @IBOutlet weak var txtOTP5: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+     }
+}
+
+extension ViewController:UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if ((textField.text?.count)! < 1 ) && (string.count > 0) {
+            if textField == txtOTP1 {
+                txtOTP2.becomeFirstResponder()
+            }
+            if textField == txtOTP2 {
+                txtOTP3.becomeFirstResponder()
+            }
+            if textField == txtOTP3 {
+                txtOTP4.becomeFirstResponder()
+            }
+            if textField == txtOTP4 {
+                txtOTP5.becomeFirstResponder()
+            }
+            if textField == txtOTP5 {
+                txtOTP5.resignFirstResponder()
+            }
+            textField.text = string
+            return false
+        } else if ((textField.text?.count)! >= 1) && (string.count == 0) {
+            if textField == txtOTP2 {
+                txtOTP1.becomeFirstResponder()
+            }
+            if textField == txtOTP3 {
+                txtOTP2.becomeFirstResponder()
+            }
+            if textField == txtOTP4 {
+                txtOTP3.becomeFirstResponder()
+            }
+            if textField == txtOTP5 {
+                txtOTP4.becomeFirstResponder()
+            }
+            if textField == txtOTP1 {
+                txtOTP1.resignFirstResponder()
+            }
+            textField.text = ""
+            return false
+        } else if (textField.text?.count)! >= 1 {
+            textField.text = string
+            return false
+        }
+        return true
+    }
+}
